393256a92dcacf76405acd0389231fa228d29cd4
genaroibc <bonavitaismael@gmail.com>
2023-07-06 18:21:10 -0300

    [FEAT]: add Task contract

---
+++ TaskContract.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract TaskContract {
+    struct Task {
+        string title;
+        uint id;
+        string description;
+    }
+
+    Task[] public tasks;
+
+    uint nextTaskId = 0;
+
+    function createTask(string memory _title, string memory _description) public {
+       tasks.push(Task({
+           description: _description,
+           id: nextTaskId++,
+           title: _title
+       }));
+    }
+
+    function getTasks(uint _taskId) public view returns (string memory, uint, string memory) {
+        uint index = findIndex(_taskId);
+
+        return (tasks[index].description,tasks[index].id,tasks[index].title);
+    }
+
+    function findIndex(uint _id) internal view returns (uint) {
+        for(uint i; i < tasks.length; i++) {
+            if(tasks[i].id == _id) return i;
+        }
+
+
+        revert("Task not found");
+    }
+
+
+    function updateTask(uint _taskId, string memory _title, string memory _description) public {
+        uint index = findIndex(_taskId);
+
+        tasks[index].title = _title;
+        tasks[index].description = _description;
+    }
+
+    function deleteTask(uint _taskId) public {
+        uint index = findIndex(_taskId);
+
+        delete tasks[index];
+    }
+}
